06921e3f03555da14ae89d808e64978333040af1
a <stepanovsm8@gmail.com>
2024-06-01 20:03:22 +0300

    stack added to VM

--- vm.c
+++ vm.c
@@ -6,38 +6,62 @@
 
 VM vm;
 
+static void resetStack()
+{
+    vm.stackTop = vm.stack;
+}
+
 void initVM()
 {
+    resetStack();
 }
 
 void freeVM()
 {
 }
 
+void push(Value value)
+{
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
 
-static InterpretResult run() {
+Value pop()
+{
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
+static InterpretResult run()
+{
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define READ_CONSTANT_LONG() (vm.chunk->constants.values[(READ_BYTE() << 16) | (READ_BYTE() << 8) | READ_BYTE()])
 
     for (;;)
     {
-        #ifdef DEBUG_TRACE_EXECUTION
+#ifdef DEBUG_TRACE_EXECUTION // enable debug trace if macro is defined
+        printf("          ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
+        {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
         disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
-        #endif
+#endif
         uint8_t instruction;
         switch (instruction = READ_BYTE())
         {
         case OP_CONSTANT_LONG:
             Value constant = READ_CONSTANT_LONG();
-            printValue(constant);
-            printf("\n");
+            push(constant);
             break;
         case OP_CONSTANT:
         {
             Value constant = READ_CONSTANT();
-            printValue(constant);
-            printf("\n");
+            push(constant);
             break;
         }
         case OP_RETURN:
@@ -49,7 +73,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
-#undef READ_CONSTANT_LONG  
+#undef READ_CONSTANT_LONG
 }
 
 InterpretResult interpret(Chunk *chunk)
--- vm.h
+++ vm.h
@@ -2,6 +2,9 @@
 #define iii_vm_h
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 1024
 
 typedef enum {
  INTERPRET_OK,
@@ -12,11 +15,16 @@ typedef enum {
 typedef struct {
     Chunk* chunk;
     uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
 } VM;
 
 void initVM();
 void freeVM();
 
 InterpretResult interpret(Chunk* chunk);
+
+void push(Value value);
+Value pop();
 
 #endif
