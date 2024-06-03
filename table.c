d90a7f480e01074cd212e9691527cef8358fc76d
a <stepanovsm8@gmail.com>
2024-06-03 15:39:48 +0300

    Hash tables added

---
+++ table.c
@@ -0,0 +1,122 @@
+#include <stdlib.h>
+#include <string.h>
+#include "memory.h"
+#include "object.h"
+#include "table.h"
+#include "value.h"
+
+#define TABLE_MAX_LOAD 0.75
+
+
+void initTable(Table *table) {
+    table->count = 0;
+    table->capacity = 0;
+    table->entries = NULL;
+}
+
+void freeTable(Table *table) {
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    initTable(table);
+}
+
+static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
+    uint32_t index = key->hash % capacity;
+    Entry *tombstone = NULL;
+
+    for (;;) {
+        Entry *entry = &entries[index];
+
+        if (entry->key == NULL) {
+            if (IS_NIL(entry->value)) {
+                // empty entry
+                return tombstone != NULL ? tombstone : entry;
+            } else {
+                // tombstone
+                if (tombstone == NULL) tombstone = entry;
+            }
+        } else if (entry->key == key) {
+            // found the key
+            return entry;
+        }
+
+        index = (index + 1) % capacity;
+    }
+}
+
+static void adjustCapacity(Table *table, int capacity) {
+    Entry *entries = ALLOCATE(Entry, capacity);
+
+    // recalculating the count because we are going to
+    // re-insert all the entries without the tombstones
+    table->count = 0;
+
+
+    for (int i = 0; i < capacity; i++) {
+        entries[i].key = NULL;
+        entries[i].value = NIL_VAL;
+    }
+
+    // rebuilding the table from scratch by re-inserting
+    // every entry into the new empty array
+    for (int i = 0; i < table->capacity; i++) {
+        Entry* entry = &table->entries[i];
+        if (entry->key == NULL) continue;
+        Entry* dest = findEntry(entries, capacity, entry->key);
+        dest->key = entry->key;
+        dest->value = entry->value;
+        table->count++;
+    }
+
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    table->entries = entries;
+    table->capacity = capacity;
+}
+
+void tableAddAll(Table *from, Table *to) {
+    for (int i = 0; i < from->capacity; i++) {
+        Entry *entry = &from->entries[i];
+        if (entry->key != NULL) {
+            tableSet(to, entry->key, entry->value);
+        }
+    }
+}
+
+bool tableDelete(Table *table, ObjString *key) {
+    if (table->count == 0) return false;
+
+    Entry *entry = findEntry(table->entries, table->capacity, key);
+    if (entry->key == NULL) return false;
+
+    // place a tombstone in the entry
+    // null key and boolean true value
+    entry->key = NULL;
+    entry->value = BOOL_VAL(true);
+
+    return true;
+}
+
+bool tableGet(Table *table, ObjString *key, Value *value) {
+    if (table->entries == NULL) return false;
+
+    Entry *entry = findEntry(table->entries, table->capacity, key);
+    if (entry->key == NULL) return false;
+
+    *value = entry->value;
+    return true;
+}
+
+bool tableSet(Table *table, ObjString *key, Value value) {
+    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+        int capacity = GROW_CAPACITY(table->capacity);
+        adjustCapacity(table, capacity);
+    }
+
+    Entry *entry = findEntry(table->entries, table->capacity, key);
+
+    bool isNewKey = entry->key == NULL;
+    if (isNewKey && IS_NIL(entry->value)) table->count++;
+
+    entry->key = key;
+    entry->value = value;
+    return isNewKey;
+}
---
+++ table.h
@@ -0,0 +1,29 @@
+// Hash table implementation for storing key-value pairs
+
+#ifndef iii_table_h
+#define iii_table_h
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+    ObjString * key;
+    Value value;
+} Entry;
+
+typedef struct  {
+    int count;
+    int capacity;
+    Entry *entries;
+} Table;
+
+void initTable(Table *table);
+void freeTable(Table *table);
+
+bool tableSet(Table *table, ObjString *key, Value value);
+bool tableGet(Table *table, ObjString *key, Value *value);
+bool tableDelete(Table* table, ObjString* key);
+
+void tableAddAll(Table *from, Table *to);
+
+#endif //iii_table_h
