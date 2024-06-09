ea8460b1a1e6173182dd23161fedbadc7d162129
a <stepanovsm8@gmail.com>
2024-06-09 11:47:18 +0300

    For loops added

--- src/compiler.c
+++ src/compiler.c
@@ -462,19 +462,85 @@ static void whileStatement()
     emitByte(OP_POP);
 }
 
+static void forStatement()
+{
+
+    beginScope();
+
+    // initializer clause
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'");
+    if (match(TOKEN_SEMICOLON))
+    {
+        // No initializer.
+    }
+    else if (match(TOKEN_VAR))
+    {
+        varDeclaration();
+    }
+    else
+    {
+        expressionStatement();
+    }
+
+    int loopStart = currentChunk()->count;
+
+    // condition clause
+    int exitJump = -1;
+    if (!match(TOKEN_SEMICOLON))
+    {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition");
+        // jump out of the loop if the condition is false
+        exitJump = emitJump(OP_JUMP_FALSE);
+        emitByte(OP_POP);
+    }
+
+    consume(TOKEN_SEMICOLON, "Expect ';'");
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses");
+
+    // increment clause
+    if (!match(TOKEN_RIGHT_PAREN))
+    {
+        int bodyJump = emitJump(OP_JUMP);
+        int incrementStart = currentChunk()->count;
+        expression();
+        emitByte(OP_POP);
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses");
+        emitLoop(loopStart);
+        loopStart = incrementStart;
+        patchJump(bodyJump);
+    }
+
+    statement();
+
+    emitLoop(loopStart);
+
+    if (exitJump != -1)
+    {
+        patchJump(exitJump);
+        emitByte(OP_POP);
+    }
+
+    endScope();
+}
+
 static void statement()
 {
-    if (match(TOKEN_FOR)) // FIXME: for now FOR works as print
-    {
-        printStatement();
-    }
-    else if (match(TOKEN_IF))
+    // if (match(TOKEN_PRI)) // FIXME: for now FOR works as print
+    // {
+    //     printStatement();
+    // }
+    if (match(TOKEN_IF))
     {
         ifStatement();
     }
     else if (match(TOKEN_WHILE))
     {
         whileStatement();
+    }
+    else if (match(TOKEN_FOR))
+    {
+        forStatement();
     }
     else if (match(TOKEN_LEFT_BRACE))
     {
