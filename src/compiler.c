4c2619a9e4916c406c78a12b7fb7df4ecb74945d
a <stepanovsm8@gmail.com>
2024-06-26 20:26:06 +0300

    this

--- src/compiler.c
+++ src/compiler.c
@@ -21,7 +21,7 @@ typedef struct {
 
 Parser parser;
 
-typedef enum { TYPE_FUNCTION, TYPE_SCRIPT } FunctionType;
+typedef enum { TYPE_FUNCTION, TYPE_METHOD, TYPE_SCRIPT } FunctionType;
 
 typedef struct Compiler {
   struct Compiler *enclosing;
@@ -37,7 +37,13 @@ typedef struct Compiler {
   int scopeDepth;
 } Compiler;
 
+typedef struct ClassCompiler {
+  struct ClassCompiler *enclosing;
+  Token name;
+} ClassCompiler;
+
 Compiler *current = NULL;
+ClassCompiler *currentClass = NULL;
 
 static Chunk *currentChunk() { return &current->function->chunk; }
 
@@ -61,9 +67,15 @@ static void initCompiler(Compiler *compiler, FunctionType type) {
 
   Local local;
   local.depth = 0;
-  local.name.start = "";
-  local.name.length = 0;
   local.isCaptured = false;
+
+  if (type != TYPE_SCRIPT) {
+    local.name.start = "this";
+    local.name.length = 4;
+  } else {
+    local.name.start = "";
+    local.name.length = 0;
+  }
 
   writeLocalsArray(&compiler->locals, local);
 }
@@ -426,7 +438,7 @@ static void method() {
   consume(TOKEN_IDENTIFIER, "Expect method name");
   uint16_t constant = identifierConstant(&parser.previous);
 
-  FunctionType type = TYPE_FUNCTION;
+  FunctionType type = TYPE_METHOD;
   function(type);
 
   emitByte(OP_METHOD);
@@ -444,6 +456,11 @@ static void classDeclaration() {
   emitShort(nameConstant);
   defineVar(nameConstant);
 
+  ClassCompiler classCompiler;
+  classCompiler.name = parser.previous;
+  classCompiler.enclosing = currentClass;
+  currentClass = &classCompiler;
+
   namedVar(className, false);  // push class name
 
   consume(TOKEN_LEFT_BRACE, "Expect '{' before class body");
@@ -452,6 +469,8 @@ static void classDeclaration() {
   }
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body");
   emitByte(OP_POP);  // pop class name
+
+  currentClass = currentClass->enclosing;
 }
 
 static void fnDeclaration() {
@@ -770,6 +789,15 @@ static void string(bool canAssign) {
 }
 static void variable(bool canAssign) { namedVar(parser.previous, canAssign); }
 
+static void this_(bool canAssign) {
+  if (currentClass == NULL) {
+    error("Can't use 'this' outside of a class");
+    return;
+  }
+
+  variable(false);
+}
+
 ParseRule rules[] = {
     [TOKEN_LEFT_PAREN] = {grouping, call, PREC_CALL},
     [TOKEN_RIGHT_PAREN] = {NULL, NULL, PREC_NONE},
@@ -803,7 +831,7 @@ ParseRule rules[] = {
     [TOKEN_OR] = {NULL, or_, PREC_OR},
     [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
     [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
-    [TOKEN_THIS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_THIS] = {this_, NULL, PREC_NONE},
     [TOKEN_TRUE] = {literal, NULL, PREC_NONE},
     [TOKEN_VAR] = {NULL, NULL, PREC_NONE},
     [TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
