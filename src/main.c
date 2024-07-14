8fa86522ada2491c72d30695b1a64e3efdaf7813
a <stepanovsm8@gmail.com>
2024-07-14 10:13:30 +0300

    file.h

--- src/main.c
+++ src/main.c
@@ -5,26 +5,8 @@
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
+#include "file.h"
 
-static char *readFile(const char *path) {
-  FILE *file = fopen(path, "rb");
-  fseek(file, 0L, SEEK_END);
-  size_t fileSize = ftell(file);
-  rewind(file);
-  char *buffer = (char *)malloc(fileSize + 1);
-  size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
-  buffer[bytesRead] = '\0';
-  fclose(file);
-  return buffer;
-}
-
-static void runFile(const char *path) {
-  char *source = readFile(path);
-  InterpretResult result = interpret(source);
-  free(source);
-  if (result == INTERPRET_COMPILE_ERROR) exit(1);
-  if (result == INTERPRET_RUNTIME_ERROR) exit(1);
-}
 
 static void repl() {
   char line[1024];
